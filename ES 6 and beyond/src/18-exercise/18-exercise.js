88e2bfe8d90edec057de8720e9f2b094b8ed7399
https://github.com/Lindarosado16 <https://github.com/Lindarosado16>
2022-10-21 14:56:40 +0200

    term

--- ES 6 and beyond/src/18-exercise/18-exercise.js
+++ ES 6 and beyond/src/18-exercise/18-exercise.js
@@ -21,7 +21,7 @@
 // Finish the code of the function
 async function exercise18() {
 fetch("https://jsonplaceholder.typicode.com/users/1")
-.then(resp=> resp.json()).then(displayContents)
+    .then(resp=> resp.json()).then(displayContents)
 }
 // Don’t change the code bellow this line
 function displayContents(data) {
